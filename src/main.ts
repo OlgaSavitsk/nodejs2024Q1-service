5d899e9b8c6ae8e0e59345f6afe47aca312e243a
Olga <olbonya.89@mail.ru>
2024-03-10 13:45:14 +0300

    feat: fix OpenAPI file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import * as yaml from 'yaml';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = await readFile(join(__dirname, '..', 'doc/api.yaml'), 'utf8');
+  const swaggerDocument = yaml.parse(file);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+
   const PORT = parseInt(process.env.PORT, 10) || 5000;
   await app.listen(PORT);
   console.log(`Server started on port ${PORT}`);
